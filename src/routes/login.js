86293f8bb8c2127220784b32e756dd6c687ab7f1
devyyj <devyyj@gmail.com>
2023-09-07 15:49:10 +0900

    구글 로그인 주석 처리

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,9 +4,9 @@ const Login = () => {
   return (
         <div className={'login-container'}>
             <h1>1초만에 로그인 🚀</h1>
-            <a href={`${process.env.REACT_APP_BACK_URL}/oauth2/authorization/google`}>
-                <img className={'login-button'} src="/google_btn.png" alt="Google 로그인"/>
-            </a>
+            {/* <a href={`${process.env.REACT_APP_BACK_URL}/oauth2/authorization/google`}> */}
+            {/*    <img className={'login-button'} src="/google_btn.png" alt="Google 로그인"/> */}
+            {/* </a> */}
             <a href={`${process.env.REACT_APP_BACK_URL}/oauth2/authorization/kakao`}>
                 <img className={'login-button'} src="/kakao_btn.png" alt="카카오 로그인"/>
             </a>
